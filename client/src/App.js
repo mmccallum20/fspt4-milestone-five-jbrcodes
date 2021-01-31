59bc7c21710a5ea430a6c641c7dc6e0504cc8a1c
mmccallum20 <mccallummsc@gmail.com>
2021-01-31 03:40:03 +0000

    Form created with first and last name fields and button

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-
 export default function App() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
   let [students, setStudents] = useState([]);
 
   useEffect(() => {
@@ -10,7 +11,7 @@ export default function App() {
   }, []);
 
   const getStudents = () => {
-    fetch('/students')
+    fetch("/students")
       .then(response => response.json())
       .then(students => {
         setStudents(students);
@@ -18,11 +19,27 @@ export default function App() {
       .catch(error => {
         console.log(error);
       });
-  }
+  };
 
   return (
     <div className="App">
       <h1>CodeOp's Facebook</h1>
+      <h2>Friends List</h2>
+      <ul>
+        {students &&
+          students.map(s => (
+            <li key={s.id}>
+              {s.firstname} {s.lastname}
+            </li>
+          ))}
+      </ul>
+      <form>
+        <label>First Name</label>
+        <input className="firstname" type="text"></input>
+        <label>Last Name</label>
+        <input className="lastname" type="text"></input>
+        <button type="button">Add Friend</button>
+      </form>
     </div>
   );
 }
